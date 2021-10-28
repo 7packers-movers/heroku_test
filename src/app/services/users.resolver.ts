418a92b373984ad0a7f5d6c25c6d0e5d79da5303
madhankumar2211 <madhankumar199722@gmail.com>
2021-10-28 11:14:26 +0530

    first

--- src/app/services/users.resolver.ts
+++ src/app/services/users.resolver.ts
@@ -7,6 +7,7 @@ import {
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { UsersService } from '../services/users.service';
+import { catchError } from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,12 @@ export class UsersResolver implements Resolve<boolean> {
   constructor(private http: HttpClient,public uS:UsersService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-    return this.http.get("/users/user")
-
+    return this.http.get("/users/user").pipe(
+      catchError(err => {
+        console.error(err);
+        alert('Your session is expired.Please login again')
+        return of(null)
+      })
+    )
   }
 }
